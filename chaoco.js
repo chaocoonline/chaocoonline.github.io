41e948684ae3cd4507d545e3063fd13f3a673d5e
Hien Doan <hienpvhc@gmail.com>
2020-03-28 16:47:57 +0700

    updated

--- chaoco.js
+++ chaoco.js
@@ -1,10 +1,16 @@
 var buttonchaoco = document.getElementById("buttonchaoco");
 buttonchaoco.addEventListener("click", function() {
     document.getElementById("flag").style.marginTop = "70px";
-  
-        var audio = document.getElementById("audio");
-        audio.play();
+    var trong = document.getElementById("trong");
+    trong.play();
+    
       
 }
  
 )
+
+var buttonquocca = document.getElementById("buttonquocca");
+buttonquocca.addEventListener("click", function(){
+    var quocca = document.getElementById("quocca");
+    quocca.play();
+})
